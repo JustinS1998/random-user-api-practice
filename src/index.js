0abc393512aa0288aef8a5e712a5570623340c6d
Justin Seitz <justinseitz2016@gmail.com>
2021-06-21 16:59:15 -0700

    now appends new user to a list and displays them

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import { getByDisplayValue } from '@testing-library/dom';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 function App(props) {
-    const [user, setUser] = useState({
-        first: '',
-        last: '',
-        img: ''
-    });
+    const [userList, setUserList] = useState([
+        {
+            first:'',
+            last:'',
+            img:''
+        }
+    ])
 
     const handleClick = () => {
         fetch('https://randomuser.me/api/?results=1')
             .then(response => response.json())
             .then(data => {
-                console.log(data.results[0]);
+                // console.log(data.results[0]);
                 const myUser = data.results[0];
-                setUser({first:myUser.name.first, last:myUser.name.last, img:myUser.picture.thumbnail});
+                setUserList([
+                    ...userList,
+                    {
+                        first:myUser.name.first,
+                        last:myUser.name.last,
+                        img:myUser.picture.thumbnail
+                    }
+                ]);
             })
             .catch(error => console.error(error));
     };
@@ -24,8 +34,14 @@ function App(props) {
         <>
             <p>Hello</p>
             <button onClick={handleClick} style={{display:'block'}}>Get User</button>
-            <img src={user.img}></img>
-            <p>{`${user.first} ${user.last}`}</p>
+            {userList.map((element, idx) => {
+                return (
+                    <div className='black-border' key={`${element.first}${element.last}`}>
+                        <img src={element.img}></img>
+                        <p>{`${element.first} ${element.last}`}</p>
+                    </div>
+                );
+            })}
         </>
     );
 }
