2fec9594fd200f3ab2457b32570aa95c161243ce
Justin Seitz <justinseitz2016@gmail.com>
2021-06-21 16:29:42 -0700

    button now displays random user json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App(props) {
+    const [user, setUser] = useState('');
+
+    const handleClick = () => {
+        fetch('https://randomuser.me/api')
+            .then(response => response.json())
+            .then(data => {
+                console.log(JSON.stringify(data.results[0]));
+                setUser(JSON.stringify(data.results[0]));
+            })
+            .catch(error => console.error(error));
+    };
+
     return (
         <>
             <p>Hello</p>
+            <button onClick={handleClick}>Get User</button>
+            <p>{user}</p>
         </>
     );
 }
